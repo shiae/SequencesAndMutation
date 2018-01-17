ced0b99b8c9f6b49d9a0dd351ea71d9f2bea3a0e
Allison Shi <shiae@rose-hulman.edu>
2018-01-17 16:40:08 -0500

    m2 DONE: 4

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -17,9 +17,9 @@ Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
 def main():
     """ Calls the   TEST   functions in this module. """
     # run_test_shortest_string()
-    run_test_index_of_largest_number()
+    # run_test_index_of_largest_number()
     # run_test_number_of_stutters()
-    # run_test_is_palindrome()
+    run_test_is_palindrome()
     # run_test_count_same()
 
 
@@ -252,9 +252,15 @@ def number_of_stutters(s):
        :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+
+    count = 0
+    for k in range(1, len(s)):
+        if s[k - 1] == s[k]:
+            count = count + 1
+    return count
 
 
 def run_test_is_palindrome():
