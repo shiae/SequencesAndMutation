bf731a8f38e7261159ffce393a80b286f07f7e51
Allison Shi <shiae@rose-hulman.edu>
2018-01-17 16:43:13 -0500

    m2 DONE: 5

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -331,7 +331,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -341,6 +341,10 @@ def is_palindrome(s):
     ####################################################################
     # ------------------------------------------------------------------
 
+    for k in range(len(s) // 2):
+        if s[k] != s[-(k + 1)]:
+            return False
+    return True
 
 # ----------------------------------------------------------------------
 # Some problems loop (iterate) through two or more sequences
