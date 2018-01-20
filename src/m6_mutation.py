5f8750c621dbf456846f1dfe1a3a64fe61a00607
Allison Shi <shiae@rose-hulman.edu>
2018-01-19 22:11:00 -0500

    m6 DONE:2

--- src/m6_mutation.py
+++ src/m6_mutation.py
@@ -4,8 +4,8 @@ In this module, you mutate by CHANGING elements of a list.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Allison Shi.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # ----------------------------------------------------------------------
 # STUDENTS:
@@ -16,7 +16,7 @@ Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
 
 def main():
     run_test_RETURN_replace_negatives_by_zeros()
-    run_test_MUTATE_replace_negatives_by_zeros()
+    # run_test_MUTATE_replace_negatives_by_zeros()
 
 
 def run_test_RETURN_replace_negatives_by_zeros():
@@ -129,10 +129,18 @@ def RETURN_replace_negatives_by_zeros(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
+
+    new_numbers = []
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            new_numbers = new_numbers + [0]
+        else:
+            new_numbers = new_numbers + [numbers[k]]
+    return new_numbers
 
 
 def run_test_MUTATE_replace_negatives_by_zeros():
